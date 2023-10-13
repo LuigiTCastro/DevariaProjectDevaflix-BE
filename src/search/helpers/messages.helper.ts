befdef97de28fc9e10d258140f8bbc1acec7493b
LuigiTCastro <luigicastro29@gmail.com>
2023-10-13 12:07:51 -0300

    adjusting

--- src/search/helpers/messages.helper.ts
+++ src/search/helpers/messages.helper.ts
@@ -1,8 +1,8 @@
 export const MovieMessagesHelper = {
     INVALID_MOVIE_PARAM: "Parametro de busca inválido.",
     MOVIE_NOT_FOUND:"Filme não encontrado.",
-    NO_RESULTS_FOUND:"Sua Busca Não resultou nada.",
+    NO_RESULTS_FOUND:"Sua Busca não resultou nada.",
     FILTER_MUST_BE_SELECTED:"Por favor selecione o filtro da busca.",
-    LIKE_MOVIE_INVALID:"Não foi possível dar like no título",
-    DISLIKE_MOVIE_INVALID:"Não foi possível dar dislike no título"
+    LIKE_MOVIE_INVALID:"Não foi possível dar like no título.",
+    DISLIKE_MOVIE_INVALID:"Não foi possível dar dislike no título."
 }
